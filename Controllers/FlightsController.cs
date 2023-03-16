d2df72fb7cfff617e86afc07ad3dac0f6c50f53e
aviv9978 <85827588+aviv9978@users.noreply.github.com>
2023-03-16 15:34:35 +0200

    Revert "Revert "Changes""
    This reverts commit 3a26a0ca014d0a90e17b4f9860c253d071fd8a3c.

---
+++ Controllers/FlightsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightSimulator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightsController : ControllerBase
+    {
+    }
+}
---
+++ Migrations/20230316131734_start.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlightSimulator.Migrations
+{
+    /// <inheritdoc />
+    public partial class start : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fligts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fligts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fligts");
+        }
+    }
+}
