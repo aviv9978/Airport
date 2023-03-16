8df5ea67761ee3183fb3d33cdcee76f8826f4408
aviv9978 <85827588+aviv9978@users.noreply.github.com>
2023-03-16 15:31:26 +0200

    Changes

---
+++ Dal/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using FlightSimulator.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlightSimulator.Dal
+{
+    public class DataContext : DbContext
+    {
+        public virtual DbSet<Flight> Fligts { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+}
